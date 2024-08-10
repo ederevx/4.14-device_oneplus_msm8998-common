2f0b0968ff36679cd41ac000aecea593e0cf77ce
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-08-10 00:32:05 -0400

    msm8998-common: Add product prop for Hey Google functionality
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -121,6 +121,7 @@ TARGET_USES_MKE2FS := true
 
 # Properties
 TARGET_SYSTEM_EXT_PROP += $(PLATFORM_PATH)/system_ext.prop
+TARGET_PRODUCT_PROP += $(PLATFORM_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
 TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
 
