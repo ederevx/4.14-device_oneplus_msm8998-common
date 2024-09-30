c65763f124e1329cffdf30703df2069210741533
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-09-29 23:22:12 -0400

    msm8998_common: No longer override cgroups and task_profiles for schedtune
    We're planning to support UCLAMP and this is no longer needed.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- common.mk
+++ common.mk
@@ -362,9 +362,7 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
