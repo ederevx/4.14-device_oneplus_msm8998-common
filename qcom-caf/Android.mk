e83bc8c67672ae8f05f1b05d673dedd06b4ed18c
Edrick Sinsuan <evcsinsuan@gmail.com>
2025-01-13 00:23:35 +0100

    msm8998-common: qcom-caf: Remove unneeded Android.mk for display
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- qcom-caf/Android.mk
+++ qcom-caf/Android.mk
@@ -1,6 +1,5 @@
 QCOM_CAF_ROOT_PATH := $(call my-dir)
 HAL_PLATFORM := msm8998
 
-include $(QCOM_CAF_ROOT_PATH)/display/Android.mk
 include $(QCOM_CAF_ROOT_PATH)/audio/Android.mk
 include $(QCOM_CAF_ROOT_PATH)/media/Android.mk
